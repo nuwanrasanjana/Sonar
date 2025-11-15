d387c6d24b81b3d50fdb45d28a8e758f2eadb7c2
nuwan rasanjana <nuwanrasanjanahn@gmail.com>
2025-11-15 23:58:47 +0530

    c

--- Sonar/Controllers/WeatherForecastController.cs
+++ Sonar/Controllers/WeatherForecastController.cs
@@ -22,6 +22,7 @@ namespace Sonar.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var name = "nuwan";
+            var name2 = "nuwan2";
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
